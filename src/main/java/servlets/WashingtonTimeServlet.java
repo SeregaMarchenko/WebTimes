8f32bd2650b9b8933ccbe65c58ccbf8b7b206250
SeregaMarchenko <mrchnksrg1@gmail.com>
2024-01-14 13:49:52 +0300

    xml config

---
+++ src/main/java/servlets/WashingtonTimeServlet.java
@@ -0,0 +1,20 @@
+package servlets;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+public class WashingtonTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("Washington time " + LocalTime.now(ZoneId.of("America/New_York")));
+        writer.close();
+    }
+}
